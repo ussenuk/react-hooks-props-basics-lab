2d235872d04025a3d344b3e7c08b4ec8ad9e4f13
ussenuk <ukimanuka@gmail.com>
2023-11-29 15:49:59 +0300

    this labs works but test doesnt pass

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
+
+function About(props) {
+
+  const isBio=props.bio;
+
+  if (isBio===""){
+
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={user.links.github} linkedin={user.links.linkedin}/>
+      </div>
+    ); } else {
+      return (
+        <div id="about">
+          <h2>About Me</h2>
+          <p>{props.bio}</p>
+          <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+          <Links github={user.links.github} linkedin={user.links.linkedin}/>
+        </div>);
+
+    }
+
+  }
 
 export default About;
